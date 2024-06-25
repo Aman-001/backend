cdd27795d3bb2d36998818711a31c1c7bec32410
Aman-001 <amanc228d#gmail.com>
2024-06-25 22:39:01 +0530

    add user and video model

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,13 +3,20 @@ class ApiError extends error {
         statuscode,
         message = "SOMETHING WENT WRONG",
         errors = [],
-        statck = ""
-    )
-    {
+        stack = ""
+    ) {
         super(message)
         this.statuscode = statuscode
         this.errors = errors
         this.data = null
         this.message = message
     }
-    }
+}
+
+// Properly set the stack trace (if provided)
+if (stack) {
+    this.stack = stack;
+} else {
+    Error.captureStackTrace(this, this.constructor);
+}
+    
